e019319eadebd227cfcba80c080572f9144f997e
Rafael Prenholato <rafaelp@icmc.usp.br>
2025-08-12 15:27:51 -0300

    Script de preparo
    - verifica privilégios de administrador
    - executa instalação do python com winget
    - cria venv como subdiretório de onde está localizado o script
    - executa a instalação das dependência usando 'pip' e o arquivo 'requirements.txt'

--- Codigos_Necessarios/Graduacao/prepara.bat
+++ Codigos_Necessarios/Graduacao/prepara.bat
@@ -1,8 +1,24 @@
+NET SESSION >nul 2>&1
 
+IF %ERRORLEVEL% EQU 0 (
+    echo Administrator privileges detected.
+    goto :ADMIN_CODE
+) ELSE (
+    echo Attempting to elevate privileges...
+    powershell -Command "Start-Process -FilePath '%~dpnx0' -Verb RunAs"
+    exit /b
+
+    goto :CRIA_VENV
+)
+
+:ADMIN_CODE
+REM Put your administrator-level commands here
+winget install -e --id Python.Python.3.13 --scope machine
+
+
+:CRIA_VENV
+cd '%~dp0'
 python -m venv .venv
-
 call .\.venv\activate
-
 pip install -r requirements.txt -U
-
 .\.venv\deactivate
